3d01d55be46ca5ef0f73b2bb4e90327cc28826cc
unknown <sakibsafat47@gmail.com>
2024-10-20 21:43:49 +0600

    some css issue fixed

--- src/app/_components/Home/TopDiscount.jsx
+++ src/app/_components/Home/TopDiscount.jsx
@@ -8,7 +8,9 @@ const TopDiscount = () => {
       <div className="max-w-screen-xl mx-auto">
         <HeadingArea heading={"Top Discount"} icon={false} link={" "} />
         <div className="grid grid-cols-1 md:grid-cols-2 md:grid-rows-2 gap-3 md:gap-5">
-          <TopDiscountCard percentageDis={"25"} title={"Black Garlic Oil"} link={"/"} description={"Stronger and thicker hair with black garlic"} disPrice={"20.00"} normalPrice={"60.00"} imgLink={"/images/slider_medcine.png"} />
+          <TopDiscountCard percentageDis={"25"} title={"Black Garlic Oil"} link={"/"} description={"Stronger and thicker hair with black garlic"} disPrice={"20.00"} normalPrice={"60.00"} imgLink={"/images/slider_medcine.png"} imgExtraClasses={"max-h-full"} extraClass={"items-stretch  "} textExClasses={'self-center'}/>
+          <TopDiscountCard percentageDis={"25"} title={"Black Garlic Oil"} link={"/"} description={"Stronger and thicker hair with black garlic"} disPrice={"20.00"} normalPrice={"60.00"} imgLink={"/images/slider_medcine.png"}  extraClass={"row-span-1"}/>
+          <TopDiscountCard percentageDis={"25"} title={"Black Garlic Oil"} link={"/"} description={"Stronger and thicker hair with black garlic"} disPrice={"20.00"} normalPrice={"60.00"} imgLink={"/images/slider_medcine.png"} extraClass={"row-span-1"} />
         </div>
       </div>
     </div>
--- src/app/_components/ProductCard/TopDiscountCard.jsx
+++ src/app/_components/ProductCard/TopDiscountCard.jsx
@@ -2,24 +2,24 @@ import Link from 'next/link';
 import Image from "next/image";
 import React from 'react';
 
-const TopDiscountCard = ({extraClass, percentageDis, title, link, description, disPrice, normalPrice, imgLink}) => {
+const TopDiscountCard = ({extraClass, percentageDis, title, link, description, disPrice, normalPrice, imgLink, imgExtraClasses, textExClasses}) => {
     return (
-        <div className={`row-span-2 bg-slate-200 p-4 pb-0 rounded-2xl flex items-center gap-5 ${extraClass && extraClass}`}>
-            <div>
+        <div className={`row-span-2 bg-slate-200 p-4 pb-0 rounded-2xl flex justify-between items-center gap-5 ${extraClass && extraClass}`}>
+            <div className={`max-w-1/2 ${textExClasses} pb-4`}>
               <p className="text-white bg-primary text-sm px-3 py-2 rounded-3xl w-fit mb-3">
                 {percentageDis}% OFF
               </p>
               <Link href={link} className="text-slate-900 text-4xl mb-3">{title}</Link>
-              <p className="text-slate-600 capitalize mb-5">
+              <p className="text-slate-600 capitalize text-xl mb-5">
                 {description}
               </p>
               <del className="text-md text-slate-400 ">${normalPrice}</del>
               <span className="text-2xl text-slate-700 pl-2">${disPrice}</span>
             </div>
             <Image
-              width={300}
-              height={200}
-              className="w-full h-full object-cover"
+              width={200}
+              height={100}
+              className={`max-w-1/3 max-h-[200px] object-cover ${imgExtraClasses}`}
               src={imgLink}
             />
           </div>
