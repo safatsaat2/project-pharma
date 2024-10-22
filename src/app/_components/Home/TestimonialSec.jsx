6f030aa814a09933cded3f72b89e7f390c49dfe9
unknown <sakibsafat47@gmail.com>
2024-10-22 21:50:11 +0600

    testimonial section creation

---
+++ src/app/_components/Home/TestimonialSec.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import HeadingArea from '../Heading/HeadingArea';
+
+const TestimonialSec = () => {
+    return (
+        <div className='px-10 srsTrendingSection py-10 lg:py-20'>
+            <div className='max-w-screen-xl mx-auto'>
+            <HeadingArea heading="Real People Real Opinion" link="/" />  
+            </div>
+
+        </div>
+    );
+};
+
+export default TestimonialSec;
--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,7 @@ import HeroSection from "@/app/_components/Home/HeroSection";
 import TrendingProduct from "./_components/Home/TrendingProduct";
 import TopDiscount from "./_components/Home/TopDiscount";
 import TopSelling from "./_components/Home/TopSelling";
+import TestimonialSec from "./_components/Home/TestimonialSec";
 
 
 export default function Home() {
@@ -11,6 +12,7 @@ export default function Home() {
       <TrendingProduct/>
       <TopDiscount/>
       <TopSelling/>
+      <TestimonialSec/>
     </>
   );
 }
