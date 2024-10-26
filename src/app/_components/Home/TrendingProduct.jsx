270729cbbe887524eabd8fe641a05f927a7c5191
unknown <sakibsafat47@gmail.com>
2024-10-26 12:42:42 +0600

    trending products section dynamic

--- src/app/_components/Home/TrendingProduct.jsx
+++ src/app/_components/Home/TrendingProduct.jsx
@@ -1,9 +1,12 @@
+"use client"
 import React from "react";
+import { useProducts } from "@/app/Providers/ProductsProvider";
 import HeadingArea from "../Heading/HeadingArea";
 import ProductCardSm from "../ProductCard/ProductCardSm";
 
 
 const TrendingProduct = () => {
+  const {productsData} = useProducts();
   return (
     <section className="px-10 srsTrendingSection py-10 lg:py-20">
       <div className="max-w-screen-xl mx-auto">
@@ -14,7 +17,20 @@ const TrendingProduct = () => {
           icon={true}
         />
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-3 md:gap-5 ">
-            <ProductCardSm imgLink={"/images/slider_medcine.png"} ratingScore={"5.0"} title={"Sunscreen Stick 250ml"} link={"/"} disPrice={"$20.00"} normalPrice={"$60.00"}/>
+          {productsData
+            .slice(0, 4) // Limit to the first 4 items
+            .map((product) => (
+              <ProductCardSm
+                key={product.id}
+                imgLink={product.imageLink || "/images/slider_medcine.png"}
+                ratingScore={product.rating || "5.0"}
+                title={product.name || "Sunscreen Stick 250ml"}
+                link={ `/products/${product.id}`}
+                disPrice={`$${product.discountPrice || "20.00"}`}
+                normalPrice={`$${product.regularPrice || "60.00"}`}
+                btnExClasses={'!w-16'}
+              />
+            ))}
         </div>
       </div>
     </section>
